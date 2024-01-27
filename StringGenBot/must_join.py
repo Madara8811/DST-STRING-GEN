17610184cb0a8f983df26d04da93b39f1dfa5522
MADARA <156997493+Madara8811@users.noreply.github.com>
2024-01-28 00:52:53 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -24,7 +24,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url=@TEAM_DST),
+                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url={TEAM_DST}),
                             ]
                         ]
                     )
